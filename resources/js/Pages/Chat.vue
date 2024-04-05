ed8482ae113d1cb96a9fedb65f7b71f8ae385a95
Guilherme Biassio <guilherme.cruz.biassio@gmail.com>
2024-04-05 09:43:36 -0300

    create chat component

--- resources/js/Pages/Chat.vue
+++ resources/js/Pages/Chat.vue
@@ -13,21 +13,83 @@ import Welcome from '@/Components/Welcome.vue';
 
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg flex" style="min-height: 400px;">
+                <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg flex" style="min-height: 400px; max-height: 400px;">
+
                     <!-- list users -->
-                    <div class="w-3/12 bg-gray-200 bg-opacity-25 border-r border-gray-300">
-                        oi
+
+                    <div class="w-3/12 bg-gray-200 bg-opacity-25 border-r border-gray-300 overflow-y-scroll">
+                        <ul>
+                            <li class="p-6 text-lg text-gray-600 leading-7 font-semibold border-b border-gray-200 hover:bg-gray-200 hover:bg-opacity-50 hover:cursor-pointer">
+                                <p class="flex items-center">
+                                    Guilherme Biassio
+                                    <span class="ml-2 w-2 h-2 bg-blue-500 rounded-full"></span>
+                                </p>
+                            </li>
+                            <li class="p-6 text-lg text-gray-600 leading-7 font-semibold border-b border-gray-200 hover:bg-gray-200 hover:bg-opacity-50 hover:cursor-pointer">
+                                <p class="flex items-center">
+                                    Rafael Pereira
+                                    <span class="ml-2 w-2 h-2 bg-blue-500 rounded-full"></span>
+                                </p>
+                            </li>
+                            <li class="p-6 text-lg text-gray-600 leading-7 font-semibold border-b border-gray-200 hover:bg-gray-200 hover:bg-opacity-50 hover:cursor-pointer">
+                                <p class="flex items-center">
+                                    Rafael Pereira
+                                    <span class="ml-2 w-2 h-2 bg-blue-500 rounded-full"></span>
+                                </p>
+                            </li>
+                            <li class="p-6 text-lg text-gray-600 leading-7 font-semibold border-b border-gray-200 hover:bg-gray-200 hover:bg-opacity-50 hover:cursor-pointer">
+                                <p class="flex items-center">
+                                    Rafael Pereira
+                                    <span class="ml-2 w-2 h-2 bg-blue-500 rounded-full"></span>
+                                </p>
+                            </li>
+                            <li class="p-6 text-lg text-gray-600 leading-7 font-semibold border-b border-gray-200 hover:bg-gray-200 hover:bg-opacity-50 hover:cursor-pointer">
+                                <p class="flex items-center">
+                                    Rafael Pereira
+                                    <span class="ml-2 w-2 h-2 bg-blue-500 rounded-full"></span>
+                                </p>
+                            </li>
+                            <li class="p-6 text-lg text-gray-600 leading-7 font-semibold border-b border-gray-200 hover:bg-gray-200 hover:bg-opacity-50 hover:cursor-pointer">
+                                <p class="flex items-center">
+                                    Rafael Pereira
+                                    <span class="ml-2 w-2 h-2 bg-blue-500 rounded-full"></span>
+                                </p>
+                            </li>
+
+                        </ul>
                     </div>
 
                     <!-- box messages -->
-                    <div class="w-9/12">
-                        <form>
-                            <input type="text">
-                            <button>Enviar</button>
-                        </form>
+                    <div class="w-9/12 flex flex-col justify-between overflow-y-scroll">
+                        <!-- messages -->
+                        <div class="w-full p-6 flex flex-col">
+                            <div class="w-full mb-3 text-right">
+                                <p class=" inline-block p-2 rounded-md bg-indigo-300 bg-opacity-25" style="max-width: 75%;">
+                                    Olá
+                                </p>
+                                <span class="block mt-1 text-xs text-gray-500">05/04/2024 08:46</span>
+                            </div>
+                        </div>
+                        <div class="w-full p-6 flex flex-col">
+                            <div class="w-full mb-3">
+                                <p class=" inline-block p-2 rounded-md message-to-me bg-gray-300 bg-opacity-25" style="max-width: 75%;">Oi</p>
+                            </div>
+                            <span class="block mt-1 text-xs text-gray-500">05/04/2024 08:46</span>
+                        </div>
+
+                        <!-- form -->
+                        <div  class="w-full bg-gray-200 bg-opacity-25 p-6 border-t border-gray-200">
+                            <form>
+                                <div class="flex rounded-md overflow-hidden border border-gray-300">
+                                    <input type="text" class="flex-1 px-4 py-2 text-sm focus:outline-none">
+                                    <button type="submit" class="bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2">Enviar</button>
+                                </div>
+                            </form>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </AppLayout>
 </template>
+
